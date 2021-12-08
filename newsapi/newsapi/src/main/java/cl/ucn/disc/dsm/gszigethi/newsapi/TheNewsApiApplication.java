6907304cd2913dd2e1f2abf58812d95a053ac2f2
Guetti <gustavo.szigethi@alumnos.ucn.cl>
2021-12-08 04:04:12 -0300

    docs(TheNesApiApplication.java): add documentation

--- newsapi/newsapi/src/main/java/cl/ucn/disc/dsm/gszigethi/newsapi/TheNewsApiApplication.java
+++ newsapi/newsapi/src/main/java/cl/ucn/disc/dsm/gszigethi/newsapi/TheNewsApiApplication.java
@@ -3,10 +3,18 @@ package cl.ucn.disc.dsm.gszigethi.newsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The News API Application
+ * @author Gustavo Patricio Szigethi Araya
+ */
 @SpringBootApplication
 public class TheNewsApiApplication {
-
+	/**
+	 * The main starting point.
+	 * @param args to use
+	 */
 	public static void main(String[] args) {
+
 		SpringApplication.run(TheNewsApiApplication.class, args);
 	}
 
